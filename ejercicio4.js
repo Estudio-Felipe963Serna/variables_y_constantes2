eb85e4a65b2aa03302ea4d4293343d375e9d5c49
Felipe Serna <juan.serna@asys.edu.co>
2024-09-19 19:06:13 -0500

    se sube el ejercicio4

---
+++ ejercicio4.js
@@ -0,0 +1,6 @@
+const nombre = "Jhon Connor";
+const birthyear = "1995";
+const anactual = "2024";
+const resubirthyear = birthyear - anactual;
+
+console.log (`Hola ${nombre} tu edad es ${resubirthyear}`);
